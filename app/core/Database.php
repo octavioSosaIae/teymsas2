c4d8fe27e9687ca9a532a054ed35967ea9079c00
Brian Clark <131870089+BrianClark04@users.noreply.github.com>
2024-09-09 19:43:44 -0300

    actualizado

---
+++ app/core/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+class conn
+{
+
+    public function connect()
+    {
+
+        $host = "localhost";
+        $user = "root";
+        $pass = "";
+        $db = "Teymsas2";
+        $port = 3306;
+
+        $mysqli = new mysqli($host, $user, $pass, $db, $puerto);
+        return $mysqli;
+    }
+}
