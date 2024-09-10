784271aace3d2d41ff458b701bb84412d1ceb396
octavioSosaIae <168479043+octavioSosaIae@users.noreply.github.com>
2024-09-09 21:07:07 -0300

    terminar modelo usuario

--- app/api/models/User.php
+++ app/api/models/User.php
@@ -5,8 +5,8 @@ require_once "../../core/Database.php";
 class User{
 
         //  Función para registro de usuarios
-        
-    function register($username, $email, $password, $full_name, $phone)
+
+    function register($complete_name, $email, $password, $full_name, $phone)
     {
         try {
             $connection = new conn;
@@ -14,7 +14,7 @@ class User{
 
             $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 
-            $sql = "INSERT INTO users (username, password_hash, email, full_name, phone) VALUES('$username','$hashedPassword', '$email','$full_name', '$phone');";
+            $sql = "INSERT INTO users (complete_name_user, password_user, email_user, phone_user) VALUES('$complete_name','$hashedPassword', '$email','$full_name', '$phone');";
             $response = $conn->query($sql);
 
             return $response;
@@ -24,7 +24,30 @@ class User{
         }
     }
 
-    function login(){}
+    //  Función para login de usuarios
+
+    function login($email, $password)
+    {
+        try {
+            $connection = new conn;
+            $conn = $connection->connect();
+
+            $sql = "SELECT * FROM users WHERE email_user='$email';";
+
+            $response = $conn->query($sql);
+            $user = $response->fetch_assoc();
+            
+            if (!password_verify($password, $user['password_user'])) {
+                throw new Exception("Error al loguear el usuario: email o contraseña incorrecto");
+            }
+
+            return $user;
+
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
 
 
 
