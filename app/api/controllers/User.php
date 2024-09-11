d091c850e3c1b8b5f6773f3a46a6ec29818091d9
Octaviososaiae <octaviososaiae@gmail.com>
2024-09-11 14:31:19 -0300

    aa

---
+++ app/api/controllers/User.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "../models/User.php";
+
+
+$function = $_GET['function'];
+
+switch ($function) {
+
+    case "login":
+
+        break;
+
+
+    case "register":
+
+        break;
+}
+
+
+
+function login($emai, $password){
+
+    
+
+}
