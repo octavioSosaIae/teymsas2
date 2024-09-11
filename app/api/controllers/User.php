aed08d8b9549cc7b3149ba02566adc1ca9a7b266
Octaviososaiae <octaviososaiae@gmail.com>
2024-09-11 17:38:48 -0300

    falta seguir modelo controlador

--- app/api/controllers/User.php
+++ app/api/controllers/User.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once "../../core/Response.php";
 require_once "../models/User.php";
 
 
@@ -9,18 +9,256 @@ switch ($function) {
 
     case "login":
 
+        login();
+
         break;
 
 
     case "register":
+
+        register();
+
+        break;
+
+    case "getAll":
+
+        getAllUsers();
+
+        break;
+
+    case "getById":
+
+        getUsersById();
+
+        break;
+
+    case "updateWithoutPassword":
+
+        updateWithoutPass();
+
+        break;
+
+    case "updatePassword":
+
+        updatePasswordUser();
+
+        break;
+
+    case "adminCreateAdmin":
 
         break;
 }
 
 
 
-function login($emai, $password){
 
-    
+function login()
+{
+    try {
 
+        $response = new Response;
+
+
+        $user = [
+            "email_user" => $_POST['email_user'],
+            "password_user" => $_POST['password_user']
+        ];
+
+
+        (new User())->login($user['email_user'], $user['password_user']);
+
+        // Responder con el usuario logueado
+        $response->setStatusCode(200);
+        $response->setBody([
+            'success' => true,
+            'message' => 'Usuario logueado exitosamente.'
+        ]);
+    } catch (Exception $e) {
+
+        // Responder con un error
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+
+    $response->send();
+}
+
+
+
+function register()
+{
+    try {
+
+        $response = new Response;
+
+
+        $user = [
+            "complete_name_user" => $_POST['complete_name_user'],
+            "email_user" => $_POST['email_user'],
+            "password_user" => $_POST['password_user'],
+            "phone_user" => $_POST['phone_user']
+
+        ];
+
+
+        (new User())->register($user['complete_name_user'], $user['email_user'], $user['password_user'], $user['phone_user']);
+
+
+        // Responder con el usuario registrado
+        $response->setStatusCode(200);
+        $response->setBody([
+            'success' => true,
+            'message' => 'Usuario registrado exitosamente.',
+            $user
+        ]);
+    } catch (Exception $e) {
+
+        // Responder con un error
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+
+    $response->send();
+}
+
+
+function getAllUsers()
+{
+
+    try {
+
+        $response = new Response;
+
+        $users = (new User())->getAll();
+
+
+        // Responder con los usuarios obtenidos
+        $response->setStatusCode(200);
+        $response->setBody([
+            'success' => true,
+            'message' => 'Usuarios obtenidos exitosamente.',
+            'users' => $users
+        ]);
+    } catch (Exception $e) {
+
+        // Responder con un error
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+    $response->send();
+}
+
+function getUsersById()
+{
+
+    try {
+
+        $response = new Response;
+
+        $id_user = $_POST['id_user'];
+
+        $users = (new User())->getById($id_user);
+
+
+        // Responder con los usuarios obtenidos
+        $response->setStatusCode(200);
+        $response->setBody([
+            'success' => true,
+            'message' => 'Usuario obtenido exitosamente.',
+            'users' => $users
+        ]);
+    } catch (Exception $e) {
+
+        // Responder con un error
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+    $response->send();
+}
+
+function updateWithoutPass()
+{
+
+    try {
+
+        $response = new Response;
+
+
+        $user = [
+            "complete_name_user" => $_POST['complete_name_user'],
+            "email_user" => $_POST['email_user'],
+            "phone_user" => $_POST['phone_user']
+
+        ];
+
+
+        (new User())->updateWithoutPassword($user['complete_name_user'], $user['email_user'], $user['phone_user']);
+
+
+        // Responder con el usuario actualizado
+        $response->setStatusCode(200);
+        $response->setBody([
+            'success' => true,
+            'message' => 'Usuario actualizado exitosamente.'
+        ]);
+    } catch (Exception $e) {
+
+        // Responder con un error
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+
+    $response->send();
+}
+
+function updatePasswordUser()
+{
+
+    try {
+
+        $response = new Response;
+
+
+        $password = [
+            "current_password" => $_POST['current_password'],
+            "new_password" => $_POST['new_password']
+
+        ];
+
+
+        (new User())->updatePassword($password['current_password'], $password['new_password']);
+
+
+        // Responder con el usuario actualizado
+        $response->setStatusCode(200);
+        $response->setBody([
+            'success' => true,
+            'message' => 'Contraseña actualizada exitosamente.'
+        ]);
+    } catch (Exception $e) {
+
+        // Responder con un error
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+
+    $response->send();
 }
--- app/api/models/User.php
+++ app/api/models/User.php
@@ -1,12 +1,13 @@
 <?php
 require_once "../../core/Database.php";
+session_start();
 
+class User
+{
 
-class User{
+    //  Función para registro de usuarios
 
-        //  Función para registro de usuarios
-
-    function register($complete_name, $email, $password, $full_name, $phone)
+    function register($complete_name, $email, $password, $phone)
     {
         try {
             $connection = new conn;
@@ -16,11 +17,10 @@ class User{
 
 
 
-            $sql = "INSERT INTO users (complete_name_user, password_user, email_user, phone_user) VALUES('$complete_name','$hashedPassword', '$email','$full_name', '$phone');";
+            $sql = "INSERT INTO users (complete_name_user, password_user, email_user, phone_user, role_user) VALUES('$complete_name','$hashedPassword', '$email', '$phone','C');";
             $response = $conn->query($sql);
 
             return $response;
-
         } catch (Exception $e) {
             throw new Exception("Error al crear el usuario: " . $e->getMessage());
         }
@@ -38,22 +38,20 @@ class User{
 
             $response = $conn->query($sql);
             $user = $response->fetch_assoc();
-            
+
             if (!password_verify($password, $user['password_user'])) {
                 throw new Exception("Error al loguear el usuario: email o contraseña incorrecto");
             }
 
-            session_start();
             $_SESSION['id_user'] = $user['id_user'];
 
             return $user;
-
         } catch (Exception $e) {
-            throw new Exception($e->getMessage());
+            throw new Exception("Error al loguearse: " . $e->getMessage());
         }
     }
 
-    
+
     //  Función para que devuelva la informacion de todos los usuarios
 
     function getAll()
@@ -66,16 +64,17 @@ class User{
 
             $response = $conn->query($sql);
             $user = $response->fetch_all(MYSQLI_ASSOC);
-            return $user;
 
+            return $user;
+            
         } catch (Exception $e) {
-            throw new Exception($e->getMessage());
+            throw new Exception("Error al obtener los usuarios: " . $e->getMessage());
         }
     }
-    
+
     //  Función para que devuelva la informacion de todos los usuarios por ID
 
-    function getAllById($id)
+    function getById($id)
     {
         try {
             $connection = new conn;
@@ -84,33 +83,93 @@ class User{
             $sql = "SELECT * FROM users WHERE id_user = '$id';";
 
             $response = $conn->query($sql);
-            $user = $response->fetch_all(MYSQLI_ASSOC);
+            $user = $response->fetch_assoc();
             return $user;
-
         } catch (Exception $e) {
-            throw new Exception($e->getMessage());
+            throw new Exception("Error al obtener los usuarios: " . $e->getMessage());
         }
     }
 
-    
-        //  Función para actualizar los usuarios sin la contraseña
 
-         function updateWithoutPassword($complete_name, $email, $phone)
-        {
-            try {           
-                $connection = new conn;
-                $conn = $connection->connect();
-                          
-    
-                $sql = "UPDATE users SET complete_name_user = '$complete_name', email_user = '$email',  phone_user = '$phone';";
-                $response = $conn->query($sql);
-                return $response;
-            } catch (Exception) {
-                return "Error";
-            }
+    //  Función para actualizar los usuarios sin la contraseña
+
+    function updateWithoutPassword($complete_name, $email, $phone)
+    {
+        try {
+            $connection = new conn;
+            $conn = $connection->connect();
+
+
+            $id_user = $_SESSION['id_user'];
+
+            $sql = "UPDATE users SET complete_name_user = '$complete_name', email_user = '$email',  phone_user = '$phone' WHERE id_user = '$id_user';";
+            $response = $conn->query($sql);
+            return $response;
+        } catch (Exception $e) {
+            throw new Exception("Error al actualizar el usuario: " . $e->getMessage());
         }
+    }
 
 
-     
-    
+    //  Función para actualizar la contraseña
+
+
+    public function updatePassword($currentPassword, $newPassword)
+    {
+
+        try {
+            $connection = new conn;
+            $conn = $connection->connect();
+
+            $id_user = $_SESSION['id_user'];
+
+            $sql = "SELECT password_user FROM users WHERE id_user = '$id_user'";
+            $response = $conn->query($sql);
+            $result = $response->fetch_assoc();
+
+            if ($result == NULL) {
+                throw new Exception("No se encontro la contraseña del usuario");
+            } else {
+                if (!password_verify($currentPassword, $result['password_user'])) {
+                    throw new Exception("La contraseña actual no coincide");
+                }
+            }
+
+            $hashedPassword = password_hash($newPassword, PASSWORD_BCRYPT); // para hashear la contraseña 
+
+            $sql = "UPDATE users SET password_user = '$hashedPassword' WHERE id_user = '$id_user'";
+            $response = $conn->query($sql);
+
+            return $response;
+        } catch (Exception $e) {
+            throw new Exception("Error al actualizar la contraseña: " . $e->getMessage());
+        }
+    }
+
+
+
+
+    //  Función para que un admin cree a otro admin
+
+    function adminCreateAdmin($complete_name, $email, $password, $phone)
+    {
+        try {
+            $connection = new conn;
+            $conn = $connection->connect();
+
+            $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+
+
+            $sql = "INSERT INTO users (complete_name_user, password_user, email_user, phone_user, role_user) VALUES('$complete_name','$hashedPassword', '$email', '$phone','A');";
+            $response = $conn->query($sql);
+
+            return $response;
+        } catch (Exception $e) {
+            throw new Exception("Error al crear el usuario: " . $e->getMessage());
+        }
+    }
+
+    function logout()
+    {}
 }
