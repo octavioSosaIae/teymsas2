811c3c8db1162a819375020fc98f29dd2cb29306
octavioSosaIae <octaviososaiae@gmail.com>
2024-10-14 17:11:26 -0300

    controlador category y modelo conrrejido y andando

--- app/api/controllers/CategoryController.php
+++ app/api/controllers/CategoryController.php
@@ -6,28 +6,29 @@ $function = $_GET['function'];
 
 switch ($function) {
     case "create":
-        create();
+        createCategory();
         break;
 
     case "getById":
-        getById();
+        getByIdCategory();
         break;
 
     case "getAll":
-        getAll();
+        getAllCategories();
         break;
 
     case "update":
-        update();
+        updateCategory();
         break;
 
     case "delete":
-        delete();
+        deleteCategory();
         break;
 }
 
-function create() {
-  
+function createCategory()
+{
+
     try {
 
         $response = new Response;
@@ -68,18 +69,150 @@ function create() {
     $response->send();
 }
 
-function getById(){
+function getByIdCategory()
+{
 
+    try {
+
+        $response = new Response;
+
+        $id_category = $_POST['id_category'];
+
+
+        // para evitar enviar datos vacios a la base de datos
+
+        if (!empty($_POST['id_category'])) {
+
+
+            $category = (new Category)->getById($id_category);
+
+
+            // Responder con los usuarios obtenidos
+            $response->setStatusCode(200);
+            $response->setBody([
+                'success' => true,
+                'message' => 'Categoria encontrada exitosamente.',
+                'categoria' => $category
+            ]);
+        }
+    } catch (Exception $e) {
+
+        // Responder con un error
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+    $response->send();
 }
 
-function getAll(){
+function getAllCategories()
+{
 
+    try {
+
+        $response = new Response;
+
+
+
+
+        $categories = (new Category)->getAll();
+
+
+        // Responder con los usuarios obtenidos
+        $response->setStatusCode(200);
+        $response->setBody([
+            'success' => true,
+            'message' => 'Categorias encontradas exitosamente.',
+            'categorias' => $categories
+        ]);
+    } catch (Exception $e) {
+
+        // Responder con un error
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+    $response->send();
 }
 
-function update(){
+function updateCategory()
+{
 
+    try {
+
+        $response = new Response;
+
+
+        $category = [
+            "description_category" => $_POST['description_category'],
+            "id_category" => $_POST['id_category']
+        ];
+
+
+        // para evitar enviar datos vacios a la base de datos
+
+        if (!empty($_POST['description_category']) && !empty($_POST['id_category'])) {
+
+            (new Category())->update($category['description_category'], $category['id_category']);
+
+
+            // Responder con el usuario actualizado
+            $response->setStatusCode(200);
+            $response->setBody([
+                'success' => true,
+                'message' => 'Categoria actualizada exitosamente.'
+            ]);
+        }
+    } catch (Exception $e) {
+
+        // Responder con un error
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+
+    $response->send();
 }
 
-function delete(){
-    
+function deleteCategory()
+{
+
+    try {
+
+        $response = new Response;
+
+        $id_category = $_POST['id_category'];
+
+
+        // para evitar enviar datos vacios a la base de datos
+
+        if (!empty($_POST['id_category'])) {
+
+
+            $category = (new Category)->delete($id_category);
+
+
+            // Responder con los usuarios obtenidos
+            $response->setStatusCode(200);
+            $response->setBody([
+                'success' => true,
+                'message' => 'Categoria eliminada exitosamente.'
+            ]);
+        }
+    } catch (Exception $e) {
+
+        // Responder con un error
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+    $response->send();
 }
