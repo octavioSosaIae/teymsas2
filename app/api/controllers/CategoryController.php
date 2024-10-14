8168a2292f6353999cd4758af704c308b460d039
ottisosa <ottisosa10@gmail.com>
2024-10-14 16:04:10 -0300

    seguir controlador categoria

--- app/api/controllers/CategoryController.php
+++ app/api/controllers/CategoryController.php
@@ -28,13 +28,44 @@ switch ($function) {
 
 function create() {
   
-        $response = new Response();
-        $description = $_POST['description'];
-        try {
-            if (!$description) {
-                throw new Exception("Descripción es requerida");
-            }
-            }
+    try {
+
+        $response = new Response;
+
+
+        $category = [
+            "description_category" => $_POST['description_category'],
+
+        ];
+
+
+        // para evitar enviar datos vacios a la base de datos
+
+        if (!empty($_POST['description_category'])) {
+
+
+            (new Category())->create($category['description_category']);
+
+
+            // Responder con success true si todo sale bien
+            $response->setStatusCode(200);
+            $response->setBody([
+                'success' => true,
+                'message' => 'Categoria agregada con exito'
+            ]);
+        }
+    } catch (Exception $e) {
+
+        // Responder con un error
+
+        $response->setStatusCode(400); // Código de estado para solicitud incorrecta
+        $response->setBody([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+
+    $response->send();
 }
 
 function getById(){
