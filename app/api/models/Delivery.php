5331123d61fd7e50352074bc462643f7c6313612
NicoleWalser <IAEnicole05@gmail.com>
2024-10-18 13:57:31 -0300

    delivery modelo

--- app/api/models/Delivery.php
+++ app/api/models/Delivery.php
@@ -3,6 +3,22 @@ require_once dirname(__DIR__) . '../../core/Database.php';
 
 class Delivery{
 
+    public function create($id_delivery,$id_customer_order,$address_delivery,$date_delivery)
+    {
+        try {
+            $connection = new conn;
+            $conn = $connection->connect();
+            $stmt = $conn->prepare("INSERT INTO deliveries ($id_delivery,$id_customer_order,$address_delivery,$date_delivery) VALUES (?,)");
+            $stmt->bind_param("s", $name_department);
+            if ($stmt->execute()) {
+                return $name_department;
+            } else {
+                throw new Exception("Error al crear el departamento: " . $stmt->error);
+            }
+        } catch (Exception $e) {
+            throw new Exception("Error al conectar con la base de datos: " . $e->getMessage());
+        }
+    }
 
 
 
