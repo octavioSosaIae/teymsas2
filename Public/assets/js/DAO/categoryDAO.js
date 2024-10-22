ceeace1aa1edcd57ee966b59d17a7955c4a2492b
Emanuel Overlay <emanueloverlay@outlook.com>
2024-10-22 17:29:23 -0300

    update DAO.JS

--- Public/assets/js/DAO/categoryDAO.js
+++ Public/assets/js/DAO/categoryDAO.js
@@ -1,16 +1,11 @@
 import server from './server.js';
 
 export default class categoryDAO{
-    async getAll(){
-        let url =   server + '/CategoriesController.php?function=getAll';
-        let response = await fetch(url);
-        let data = await response.json();
-        return data;
-    }
 
     async createCategory(description_category){
-        let url = 'http://localhost/teymsas2/app/api/controllers/CategoriesController.php?function=create'
+        let url = server + '/CategoriesController.php?function=create'
         let formData = new FormData();
+        
         formData.append('description_category', description_category);
         let config = {
             method: 'POST',
@@ -21,5 +16,28 @@ export default class categoryDAO{
         let data = await response.json();
         return data;
     }
+
+    async getById(id_category){
+        let url =   server + '/CategoriesController.php?function=getById';
+        formData.append('id_category', id_category);
+
+        let config = {
+            method: 'POST',
+            body: formData
+        };
+
+        let response = await fetch(url);
+        let data = await response.json();
+        return data;
+    }
+
+    async getAll(){
+        let url =   server + '/CategoriesController.php?function=getAll';
+        let response = await fetch(url);
+        let data = await response.json();
+        
+        return data;
+    }
+
 
 }
