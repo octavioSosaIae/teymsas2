8592cba1e466f5f326fc9636c3c44796824da981
NicoleWalser <IAEnicole05@gmail.com>
2024-10-22 23:35:01 -0300

    dao order terminado

--- Public/assets/js/DAO/orderDAO.js
+++ Public/assets/js/DAO/orderDAO.js
@@ -2,15 +2,10 @@ import server from './server.js';
 
 export default class orderDAO{
 
-    async createOrder(id_user_customer,document_customer,address_customer,business_name_customer,rut_customer,id_city){
-        let url= server + '/CustomersController.php?function=create';
+    async createOrder(description_status){
+        let url= server + '/OrderController.php?function=create';
         let formData = new FormData();
-        formData.append('id_user_customer', id_user_customer);
-        formData.append('document_customer', document_customer);
-        formData.append('address_customer', address_customer);
-        formData.append('business_name_customer', business_name_customer);
-        formData.append('rut_customer', rut_customer);
-        formData.append('id_city', id_city);
+        formData.append('description_status', description_status);
         let config = {
             method: 'POST',
             body: formData
@@ -22,28 +17,24 @@ export default class orderDAO{
         }
 
         async getAll(){
-            let url =  server + '/CustomersController.php?function=getAll';
+            let url =  server + '/OrderController.php?function=getAll';
             let response = await fetch(url);
             let data = await response.json();
             return data;
         }
 
-        async getById(id_user){
-            let url =   server + '/CustomersController.php?function=getByid&userId'+ id_user;
+        async getById(id_order_status){
+            let url =   server + '/OrderController.php?function=getByid&order_statusId'+ id_order_status;
             let response = await fetch(url);
             let data = await response.json();
             return data;
         } 
 
-        async updateCustomer(document_customer,address_customer,businessName_customer,rut_customer,id_city_customer,id_user){
-            let url= server + '/CustomersController.php?function=update';
+        async updateOrder(description_status,id_order_status){
+            let url= server + '/OrderController.php?function=update';
             let formData = new FormData();
-            formData.append('document_customer', document_customer);
-            formData.append('address_customer', address_customer);
-            formData.append('businessName_customer', businessName_customer);
-            formData.append('rut_customer', rut_customer);
-            formData.append('id_city_customer', id_city_customer);
-            formData.append('id_user', id_user);
+            formData.append('description_status', description_status);
+            formData.append('id_order_status', id_order_status);
             let config = {
                 method: 'POST',
                 body: formData
@@ -54,10 +45,10 @@ export default class orderDAO{
             return data;
         }
 
-        async deleteCustomer(id_user){
-            let url= server + '/CustomersController.php?function=delete';
+        async deleteOrder(id_order_status){
+            let url= server + '/OrderController.php?function=delete';
             let formData = new FormData();
-            formData.append('id_user', id_user);
+            formData.append('id_order_status', id_order_status);
             let config = {
                 method: 'POST',
                 body: formData
