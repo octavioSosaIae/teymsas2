9fdfc6a0f1db3c0251118435d0b64e235b85b3f6
Octaviososaiae <octaviososaiae@gmail.com>
2024-10-25 14:07:01 -0300

    review modelo controlador

--- app/api/models/Review.php
+++ app/api/models/Review.php
@@ -1,46 +1,54 @@
 <?php
 require_once dirname(__DIR__) . '../../core/Database.php';
 
-class Review{
+class Review
+{
 
-    function create($id_review,$id_customer_order,$id_product,$id_customer,$rating_review,$comment_review,$created_at_review){
-        try{
+    function create($id_customer_order, $id_product, $rating_review, $comment_review, $created_at_review)
+    {
+        try {
+
             $connection = new conn;
             $conn = $connection->connect();
-            $stmt = $conn->prepare("INSERT INTO review ($id_review,$id_customer_order,$id_product,$id_customer,$rating_review,$comment_review,$created_at_review) VALUES( ? , ? , ? , ? , ?, ?, ?);");
-            $stmt->bind_param("sssis", $id_review,$id_customer_order,$id_product,$id_customer,$rating_review,$comment_review,$created_at_review);
-            if($stmt->execute()){
+
+            $id_customer = $_SESSION['id_user'];
+
+            $stmt = $conn->prepare("INSERT INTO product_reviews ($id_customer_order,$id_product,$id_customer,$rating_review,$comment_review,$created_at_review) VALUES( ? , ? , ? , ? , ?, ?, ?);");
+            $stmt->bind_param("sssis",  $id_customer_order, $id_product, $id_customer, $rating_review, $comment_review, $created_at_review);
+            if ($stmt->execute()) {
                 return true;
-            } else{
+            } else {
                 throw new Exception("Error al agregar el comentario: " . $stmt->error);
             }
-        }catch(Exception $e){
-            throw new Exception("Error al conectar con la base de datos: " . $e->getMessage()); 
-        }      
+        } catch (Exception $e) {
+            throw new Exception("Error al conectar con la base de datos: " . $e->getMessage());
         }
+    }
 
-    function getAll(){
-        try{
+    function getAll()
+    {
+        try {
             $connection = new conn;
             $conn = $connection->connect();
-            $stmt = $conn->prepare("SELECT * FROM review;");
-            if($stmt->execute()){
+            $stmt = $conn->prepare("SELECT * FROM product_reviews;");
+            if ($stmt->execute()) {
                 $result = $stmt->get_result();
-                $review = $result->fetch_all(MYSQLI_ASSOC);   
-            }else{
+                $review = $result->fetch_all(MYSQLI_ASSOC);
+            } else {
                 throw new Exception("Error al obtener el comentario: " . $stmt->error);
             }
-                return $review;
-            }catch(Exception $e){
-                    throw new Exception("Error al conectar con la base de datos: " . $e->getMessage());
-                }
-            }
+            return $review;
+        } catch (Exception $e) {
+            throw new Exception("Error al conectar con la base de datos: " . $e->getMessage());
+        }
+    }
 
-    function getById($id_review){
-        try{
+    function getById($id_review)
+    {
+        try {
             $connection = new conn;
             $conn = $connection->connect();
-            $stmt = $conn->prepare("SELECT * FROM review WHERE id_review = ?;");
+            $stmt = $conn->prepare("SELECT * FROM product_reviews WHERE id_review = ?;");
             $stmt->bind_param("i", $id_review);
             if ($stmt->execute()) {
                 $result = $stmt->get_result();
@@ -49,49 +57,48 @@ class Review{
                 throw new Exception("Error al obtener el comentario: " . $stmt->error);
             }
             return $review;
-            } catch (Exception $e) {
-                throw new Exception("Error al conectar con la base de datos: " . $e->getMessage());
-            }
-            }
+        } catch (Exception $e) {
+            throw new Exception("Error al conectar con la base de datos: " . $e->getMessage());
+        }
+    }
 
 
-    function update($id_review,$rating_review,$comment_review)
+    function update($rating_review, $comment_review, $id_review)
     {
         try {
             $connection = new conn;
             $conn = $connection->connect();
-            
-            $stmt = $conn->prepare("UPDATE purchase SET id_review,rating_review,comment_review = ?, ?, ? WHERE = ?, ?, ? ;");
-            $stmt->bind_param("ssii",$id_review,$rating_review,$comment_review);
+
+            $stmt = $conn->prepare("UPDATE product_reviews SET rating_review, comment_review = ?, ? WHERE id_review = ? ;");
+            $stmt->bind_param("isi", $rating_review, $comment_review, $id_review);
             if ($stmt->execute()) {
                 return true;
             } else {
                 throw new Exception("Error al actualizar el comentario: " . $stmt->error);
             }
-            } catch (Exception $e) {
-                throw new Exception("Error al conectar con la base de datos: " . $e->getMessage());
-            }
-            }               
+        } catch (Exception $e) {
+            throw new Exception("Error al conectar con la base de datos: " . $e->getMessage());
+        }
+    }
 
 
     function delete($id_review)
     {
-    try {
-        $connection = new conn;
-        $conn = $connection->connect();
 
-        $stmt = $conn->prepare("DELETE FROM review WHERE id_review = ?;");
-        $stmt->bind_param("i", $id_review);
+        try {
+            $connection = new conn;
+            $conn = $connection->connect();
 
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            throw new Exception("Error al eliminar el comentario: " . $stmt->error);
-        }
+            $stmt = $conn->prepare("DELETE FROM product_reviews WHERE id_review = ?;");
+            $stmt->bind_param("i", $id_review);
+
+            if ($stmt->execute()) {
+                return true;
+            } else {
+                throw new Exception("Error al eliminar el comentario: " . $stmt->error);
+            }
         } catch (Exception $e) {
             throw new Exception("Error al conectar con la base de datos: " . $e->getMessage());
         }
-    } 
-
-
+    }
 }
