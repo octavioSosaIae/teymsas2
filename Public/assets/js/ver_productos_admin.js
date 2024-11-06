7c3163138462277cb4d7b693346ab1e3df573276
Octaviososaiae <octaviososaiae@gmail.com>
2024-11-06 00:13:17 -0300

    falta editar y eliminar y agregar productos desde admin

--- Public/assets/js/ver_productos_admin.js
+++ Public/assets/js/ver_productos_admin.js
@@ -12,9 +12,8 @@ async function obtainProducts() {
     showProducts(data.data);
 }
 
-function showProducts(product) {
-    
-    console.log(product);
+async function showProducts(product) {
+
     let tbodyElement = document.querySelector("#mostrarProducto");
     tbodyElement.innerHTML = "";
     for (let i = 0; i < product.length; i++) {
@@ -22,16 +21,54 @@ function showProducts(product) {
         tbodyElement.innerHTML += `               
                 <tr>
                 <td>${product[i].id_product}</td>
+                <td>${product[i].description_product}</td>    
                 <td>${product[i].details_product}</td>
                 <td>${product[i].price_product}</td>
                 <td>${product[i].thumbnail_product}</td>    
                 <td>${product[i].stock_product}</td>    
                 <td>${product[i].measures_product}</td>    
-                <td>${product[i].category_product}</td>    
-
-                </tr>
+                <td>${product[i].id_category}</td>    
+        <td>
+            <select>
+                <option value="">...</option>
+                <option value="editar">Editar</option>
+                <option value="eliminar">Eliminar</option>
+            </select>
+        </td>    
+                        </tr>
                 `;
     }
 
+
+
 }
+
+function addProduct(){
+    let form = document.querySelector("#agregarProducto");
+
+    formElement.onsubmit = async (e) => {
+        e.preventDefault();
+        let fromFormData = new FormData(formElement);
+        let url = 'http://localhost/teymsas2/app/api/controllers/ProductController.php?function=create';
+
+        let config = {
+            method: 'POST',
+            body: fromFormData
+        };
+
+        let response = await fetch(url, config);
+        let data = await response.json();
+
+        console.log(data);
+
+        if (data == true) {
+            alert('Producto agregado exitosamente')
+        } else (
+            alert('Error al agregar el producto')
+        )
+    }
+   
+}
+
+
 
